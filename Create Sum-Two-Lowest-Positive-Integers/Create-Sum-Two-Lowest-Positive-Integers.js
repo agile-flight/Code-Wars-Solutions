34518728de83f7d277b740727f28248b9cfed8cb
Amartya Gunjan <53094015+agile-flight@users.noreply.github.com>
2022-03-28 12:19:21 -0500

    Create Create-Sum-Two-Lowest-Positive-Integers.js

---
+++ Create Sum-Two-Lowest-Positive-Integers/Create-Sum-Two-Lowest-Positive-Integers.js
@@ -0,0 +1,6 @@
+function sumTwoSmallestNumbers(numbers) {  
+  const Min1 = Math.min(...numbers);
+  numbers.splice(numbers.indexOf(Min1), 1);
+  const Min2 = Math.min(...numbers);
+  return Min1 + Min2;
+}
